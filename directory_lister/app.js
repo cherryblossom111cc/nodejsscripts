5be8ccbe06484a968662c9dfa516c7fe2066b073
Tim Bielawski <tim@afrozaar.com>
2014-01-06 16:53:16 +0200

    Add regex filtering

--- directory_lister/app.js
+++ directory_lister/app.js
@@ -2,9 +2,16 @@ var sys = require("sys");
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var url = require("url"); 
+var url = require("url");
 
-function dirTree(filename) {
+
+// filename - path of directory structure 
+// level - level to transverse into the directory structure -1 = inifnite
+// rule [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] some regex to match
+function dirTree(filename, level, runningLevel, rule) {
+    console.log(filename);
+    var regexPattern;
+
     var stats = fs.lstatSync(filename),
         info = {
             path: filename,
@@ -12,12 +19,30 @@ function dirTree(filename) {
         };
 
     if (stats.isDirectory()) {
-        info.type = "folder";
-        info.children = fs.readdirSync(filename).map(function(child) {
-            return dirTree(filename + '/' + child);
-        });
-    } else {
-        info.type = "file";
+            info.type = "folder";
+        }
+
+    if(runningLevel < level || level == -1){
+        if (stats.isDirectory()) {
+            info.children = fs.readdirSync(filename).map(function(child) {
+                runningLevel++;
+                 if(rule != ""){
+                    regexPattern = new RegExp(rule);
+                    if(!child.match(regexPattern)){
+                        console.log(child);
+                        console.log(rule);
+                        return null;
+                    }else{
+                       return dirTree(filename + '/' + child, level, runningLevel, rule);  
+                    }
+                }else{
+                   return dirTree(filename + '/' + child, level, runningLevel, rule); 
+                }
+                
+            });
+        } else {
+            info.type = "file";
+        }
     }
 
     return info;
@@ -35,9 +60,13 @@ http.createServer(function (req, res) {
         }  
 
         fs.readFile(filename, "binary", function(err, file) {  
-            if(err) {  
+            if(err) { 
                 res.writeHead(200, { "Content-Type": "text/plain" });
-                res.write(JSON.stringify(dirTree(uri.substring(1)), false, null)); 
+                if(uri.substring(1) != ""){
+                    res.write(JSON.stringify(dirTree(uri.substring(1), -1, 1, ""), false, null)); 
+                }else{
+                   res.write(JSON.stringify(dirTree("escenic", 2, 1, "^[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]$"), false, null));  
+                }
                 res.end();  
                 return;  
             }  
